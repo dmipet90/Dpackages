514df3de68e37e506a30b484d68e7dc0e26a4896
User <User@301-8>
2019-02-08 20:04:41 +0200

    StringBufferTest added

---
+++ src/StringBufferTest/StringBufferTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StringBufferTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author User
+ */
+public class StringBufferTest {
+    public static void main(String[] args) throws IOException {
+        // System.in - читает поток байтов
+        // InputStreamreader обертка - преобразует поток байтов в поток символов
+        // bufferedReader - считывает строки
+        BufferedReader  br = new BufferedReader (new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str.isEmpty());
+        System.out.println(str.length());
+        StringBuilder str2 = new StringBuilder(str);
+        System.out.println("Your enter: " + str);
+        System.out.println("But reverse form will be: " + str2.reverse());
+    }
+}
